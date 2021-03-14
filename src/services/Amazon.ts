7bd7d4f005269323afb5eef6bd25de231904dd69
Raisess <raisechif@gmail.com>
2021-03-14 13:48:38 -0300

    removed is dot br method

--- src/services/Amazon.ts
+++ src/services/Amazon.ts
@@ -7,10 +7,6 @@ export default class Amazon {
 		this.amazonUri = amazonUri;
 	}
 
-	protected isDotBr(): boolean {
-		return this.amazonUri.includes(".br");
-	}
-
 	protected async fetchPage(category: string, page?: number): Promise<string> {
 		const req:  any    = await fetch(`${this.amazonUri}/s?k=${category.replace(/\s+/g, "+")}&ref=nb_sb_noss${page ? "&page=" + page : ""}`);
 		const html: string = await req.text();
