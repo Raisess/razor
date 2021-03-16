c724793d59d0e2b48b424bffd9094209787a7682
Raisess <raisechif@gmail.com>
2021-03-16 11:34:05 -0300

    created a method to fetch single product page

--- src/services/Amazon.ts
+++ src/services/Amazon.ts
@@ -7,8 +7,15 @@ export default class Amazon {
 		this.amazonUri = amazonUri;
 	}
 
-	protected async fetchPage(category: string, page?: number): Promise<string> {
+	protected async fetchSearchPage(category: string, page?: number): Promise<string> {
 		const req:  any    = await fetch(`${this.amazonUri}/s?k=${category.replace(/\s+/g, "+")}&ref=nb_sb_noss${page ? "&page=" + page : ""}`);
+		const html: string = await req.text();
+
+		return html;
+	}
+
+	protected async fetchProductPage(name: string, id: string): Promise<string> {
+		const req:  any    = await fetch(`${this.amazonUri}/${name}/dp/${id}`);
 		const html: string = await req.text();
 
 		return html;
